15ae1570ad9eb3cd43dd44e566fc1804b1cdab70
João Vitor <129129458+Ultralitz-Dev@users.noreply.github.com>
2023-03-29 18:25:41 -0300

    Add files via upload

---
+++ Operações.js
@@ -0,0 +1,14 @@
+// Operação de soma
+alert(2 + 2);
+
+// Operação de subtração
+alert(4 - 2);
+
+// Operação de multiplicação
+alert(10 * 2);
+
+// Operação de divisão
+alert(5 / 5);
+        
+// Operação de média (prioridade para o que estiver dentro dos parenteses)
+ alert((9 - 3 + 4) * 5);
